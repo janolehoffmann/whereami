8030ec49db8611f482c2424ba4b609f8dafc0bab
janolehoffmann <51462206+janolehoffmann@users.noreply.github.com>
2022-01-07 16:36:44 +0100

    improve error handling

--- logic.js
+++ logic.js
@@ -17,9 +17,15 @@ function getName(position) {
 
         var contentURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&origin=*&format=json&exintro=&titles=" + placeName;
         $.getJSON(contentURL, function (contentData) {
+            var error = false;
             var query = contentData.query.pages;
             var key = Object.keys(query)[0];
             var extract = query[key].extract;
+
+            if (extract == null || extract == "") {
+                displayFactError();
+                return;
+            }
 
             var position = 0;
             var result = [];
@@ -45,9 +51,23 @@ function getName(position) {
             //get substring from start index to next period
             var fact = extract.substring(startIndex, extract.indexOf('. ', startIndex) + 1);
 
-            y.innerHTML = '<div class="content"><b>Did you know? </b><br>' + fact + '</div>';
+            if (error == true) {
+                y.innerHTML = '<div class="content"><span class="error">Sorry, could not find a fact for your location<span></div>';
+            } else {
+                y.innerHTML = '<div class="content"><b>Did you know? </b><br>' + fact + '</div>';
+            }
+        })
+        .fail(function() { 
+            displayFactError();
         });
 
         z.innerHTML = '<div class="content"><a href="https://www.google.de/maps/place/' + placeName + '" target="_blank">Find your way home!</a></div>';
+    })
+    .fail(function() { 
+        x.innerHTML = '<span class="error">Sorry, could not find your location<span>';
     });
+}
+
+function displayFactError() {
+    y.innerHTML = '<div class="content"><span class="error">Sorry, could not find a fact for your location<span></div>';
 }
